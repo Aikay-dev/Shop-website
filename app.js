110eb0191e1823d436bf12dc5d014b7bcda61dcd
Emmanuel Festus <120015643+GeneralAike@users.noreply.github.com>
2023-01-27 13:10:29 +0100

    fixed the add cart button not working

--- app.js
+++ app.js
@@ -1,22 +1,3 @@
-const cartbut = document.querySelectorAll(".addcart");
-let arrconv = Array.from(cartbut);
-arrconv.forEach(function (elem) {
-  elem.addEventListener("click", async function () {
-    this.classList.toggle("green");
-    if (this.classList.contains("green")) {
-      let red = document.querySelector(".confirm");
-      red.classList.toggle("dropdown");
-      await new Promise((resolve) => setTimeout(resolve, 3000));
-      red.classList.toggle("dropdown");
-    } else {
-      let green = document.querySelector(".confirmout");
-      green.classList.toggle("dropdown");
-      await new Promise((resolve) => setTimeout(resolve, 3000));
-      green.classList.toggle("dropdown");
-    }
-  });
-});
-
 /* phone storage */
 
 const MAINSTORAGE = [
@@ -171,3 +152,23 @@ function* randomChoice(galaxyphones) {
 for (let element of randomChoice(galaxyphones)) {
   document.querySelector(".samph").innerHTML += element;
 }
+
+
+const cartbut = document.querySelectorAll(".addcart");
+let arrconv = Array.from(cartbut);
+arrconv.forEach(function (elem) {
+  elem.addEventListener("click", async function () {
+    this.classList.toggle("green");
+    if (this.classList.contains("green")) {
+      let red = document.querySelector(".confirm");
+      red.classList.toggle("dropdown");
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      red.classList.toggle("dropdown");
+    } else {
+      let green = document.querySelector(".confirmout");
+      green.classList.toggle("dropdown");
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      green.classList.toggle("dropdown");
+    }
+  });
+});
