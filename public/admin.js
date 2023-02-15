2ccb5e841202fa9d368d6539cba9e1b670bed792
Emmanuel Festus <Emmanese2020@gmail.com>
2023-02-15 23:10:38 +0100

    added auth feature

---
+++ public/admin.js
@@ -0,0 +1,55 @@
+const hamburger = document.querySelector(".hamburger");
+const navflow = document.querySelector(".navul");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navflow.classList.toggle("navulba");
+  navflow.classList.toggle("navulba>a");
+});
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBEgf5tB55aeJSLYv50DjT38XRuZiAdrxE",
+    authDomain: "shop-site-269eb.firebaseapp.com",
+    databaseURL: "https://shop-site-269eb-default-rtdb.firebaseio.com",
+    projectId: "shop-site-269eb",
+    storageBucket: "shop-site-269eb.appspot.com",
+    messagingSenderId: "139391464564",
+    appId: "1:139391464564:web:2be388f749b78b8c04cd21",
+    measurementId: "G-CLNF0CJQEJ",
+  };
+
+
+firebase.initializeApp(firebaseConfig);
+
+
+const emailAuthProvider = firebase.auth.EmailAuthProvider;
+
+const form = document.querySelector(".formcont")
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    let email = form.email.value
+    let password = form.password.value
+    
+    firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user);
+    window.location.href = "product-management.html"
+    console.log("hsa");
+  })
+  .catch((error) => {
+    // Handle errors
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    let boxes = document.querySelector(".filform")
+    let boxes2 = document.querySelector(".ha")
+    boxes.classList.add("wrongpass")
+    boxes2.classList.add("wrongpass")
+    console.log("wrong p");
+    // ...
+  });
+    
+})
+
